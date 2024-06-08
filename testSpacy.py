0cc7413474f4d72470f5595d0d0e8660de45c355
Henrik Melin Maas <henmel43@kth.se>
2024-06-08 11:47:59 +0200

    Add mote tests

--- testSpacy.py
+++ testSpacy.py
@@ -1,6 +1,12 @@
+#import spacy
+#from spacy_streamlit import visualize_parser
+import streamlit as st
 import spacy
-from spacy_streamlit import visualize_parser
 
+# Load SpaCy model
+nlp = spacy.load('en_core_web_sm')
+
+st.title('SpaCy with Streamlit  en_core_web_sm')
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("This is a text")
-visualize_parser(doc)
+#visualize_parser(doc)
