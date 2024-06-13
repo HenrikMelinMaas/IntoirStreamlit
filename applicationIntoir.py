cea8fff20741ef6b34f4b93b1cd63245df507d12
Henrik Melin Maas <henmel43@kth.se>
2024-06-13 15:13:48 +0200

    new wait txt 270 s

--- applicationIntoir.py
+++ applicationIntoir.py
@@ -1,3 +1,4 @@
+#
 # applicationIntoir.py 
 # v004
 #
@@ -23,14 +24,14 @@ input_text = st.text_area(" Intoir your work situation v004:")
 
 # Check if the input text length is greater than 100 characters
 if len(input_text) > 100:
-    st.success("Lets proceed!")
+    st.success("Your answer will now be weighed against 270 hierarchy statements. The first three levels of the hierarchy are based on psychometric research definitions, while the lower part, which forms the bulk of the statements, is derived from Mistal et al")
 else:
     st.warning("Please enter at least 60 words")
 
 # Add a button to simulate an action that should only proceed if the condition is met
 if st.button("OK"):
     if len(input_text) > 100:
-        st.write("Proceeding with the action...")
+        st.write("Wait...")
         # Add your logic here for what happens when the user clicks OK
     else:
         st.error("The text must be long.")
@@ -38,7 +39,6 @@ if st.button("OK"):
 if len(input_text) < 100:
     input_text = "And Crispin Crispian shall neer go by, From this day to the ending of the world, But we in it shall be remember We few, we happy few, we band of brothers"
 
-    
 # The
 recommendations, mistral_out, mistral_score, construct_score = main_fx (input_text) 
 
